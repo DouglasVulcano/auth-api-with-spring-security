00de1d44d38a8c3372d853665d43d61ca0a8a315
DouglasVulcano <vulcanodouglas1708@gmail.com>
2024-11-03 05:47:27 -0300

    impl security filter class

--- src/main/java/auth/app/api/repositories/UserRepository.java
+++ src/main/java/auth/app/api/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package auth.app.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import auth.app.api.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
