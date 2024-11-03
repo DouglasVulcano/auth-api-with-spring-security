5d1d4bd21c44aa40d4337099bd170b6a58108d63
DouglasVulcano <vulcanodouglas1708@gmail.com>
2024-11-03 04:57:34 -0300

    start project

---
+++ src/test/java/auth/app/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package auth.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
